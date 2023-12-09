505c1d435323198f5545bb896c31682283d7f9ee
Moe <masihdon@hotmail.de>
2023-12-09 21:26:22 +0100

    test

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import Login from './index.js';
 
 
 describe('Login component', () => {
-test('Toggling login state', () => {
+test('Toggling login state', async () => {
     render(<Login login={false} handleLogin={() => {}} />)
     const text = screen.getByText(/Log in/i)
     expect(text).toBeInTheDocument();
-    fireEvent.click(screen.getByText((/Log in/i)))
-    // const updatedState = screen.getByText((/Log out/i))
-    expect(screen.getByText(/Log out/i)).toBeInTheDocument()
+    fireEvent.click(screen.getByText((/Log in/i)));
     
-
+    await waitFor(() => {
+        console.log(document.body.innerHTML);
+ expect(screen.getByText(/Log out/i)).toBeInTheDocument()
+    });
+  
 });
 })
